456d61c3645f8d8781d40701bd3a4461755efd98
Zoran Stojsavljevic <zoran.stojsavljevic@gmail.com>
2018-08-24 15:23:24 +0200

    Both client.c and server.c are upgraded to reflect send() and recv() functions
    while loop added for both client.c and server.c to simultaneously send() and recv() data in between.

--- socketTCPIP/client.c
+++ socketTCPIP/client.c
@@ -1,50 +1,79 @@
 /****************** CLIENT CODE ****************/
+// https://www.binarytides.com/server-client-example-c-sockets-linux/
 
 #include <stdio.h>
+#include <string.h>
+#include <unistd.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
-#include <string.h>
 
 int main(){
-	int clientSocket;
-	char buffer[1024];
-	struct sockaddr_in serverAddr;
-	socklen_t addr_size;
+    int clientSocket;
+    char buffer[1024];
+    struct sockaddr_in serverAddr;
+    socklen_t addr_size;
 
-	bzero(&buffer, sizeof(buffer));
+    bzero(&buffer, sizeof(buffer));
 
-	/*---- Create the socket. The three arguments are: ----*/
-	/* 1) Internet domain 2) Stream socket 3) Default protocol IPPROTO_IP (IP in this case) */
-	// clientSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_IP);
-	/* 1) Internet domain 2) Stream socket 3) IPPROTO_TCP (TCP in this case) */
-	clientSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-	/* 1) Internet domain 2) Stream socket 3) IPPROTO_UDP (UDP in this case) */
-	// clientSocket = socket(PF_INET, SOCK_STREAM, IPPROTO_UDP);
+    /*---- Create the socket. The three arguments are: ----*/
+    /* 1) Internet domain 2) Stream socket 3) Default protocol IPPROTO_IP (IP in this case) */
+    // clientSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_IP);
+    /* 1) Internet domain 2) Stream socket 3) IPPROTO_TCP (TCP in this case) */
+    clientSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    /* 1) Internet domain 2) Stream socket 3) IPPROTO_UDP (UDP in this case) */
+    // clientSocket = socket(PF_INET, SOCK_STREAM, IPPROTO_UDP);
 
-	/*---- Configure settings of the server address struct ----*/
-	/* Address family = Internet */
-	serverAddr.sin_family = AF_INET;
-	/* Set port number, using htons function to use proper byte order */
-	serverAddr.sin_port = htons(7891);
-	/* Set IP address to localhost */
-	//	serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
-	/* Set IP address to distant server */
-	// serverAddr.sin_addr.s_addr = inet_addr("192.168.178.28");
-	serverAddr.sin_addr.s_addr = inet_addr("172.16.71.51");
+    /*---- Configure settings of the server address struct ----*/
+    /* Address family = Internet */
+    serverAddr.sin_family = AF_INET;
+    /* Set port number, using htons function to use proper byte order */
+    serverAddr.sin_port = htons(7891);
+    /* Set IP address to localhost */
+    //	serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    /* Set IP address to distant server */
+    // serverAddr.sin_addr.s_addr = inet_addr("192.168.178.28");
+    // serverAddr.sin_addr.s_addr = inet_addr("172.16.71.51");
+    serverAddr.sin_addr.s_addr = inet_addr("192.168.1.4");
 
-	/* Set all bits of the padding field to 0 */
-	memset(serverAddr.sin_zero, '\0', sizeof serverAddr.sin_zero);
+    /* Set all bits of the padding field to 0 */
+    memset(serverAddr.sin_zero, '\0', sizeof serverAddr.sin_zero);
 
-	/*---- Connect the socket to the server using the address struct ----*/
-	addr_size = sizeof serverAddr;
-	connect(clientSocket, (struct sockaddr *) &serverAddr, addr_size);
+    /*---- Connect the socket to the remote server using the address struct ----*/
+    // addr_size = sizeof serverAddr;
+    if (connect(clientSocket, (struct sockaddr *) &serverAddr, sizeof(serverAddr)) < 0) {
+	printf("connect failed. Error");
+	return 1;
+    }
+
+    printf("Client/Server connected!\n");
+
+    int i = 0;
+
+    // Keep communicating with server
+    while(i < 7) {
+	// Prepare some data
+	strcpy(buffer, "Server IP addr: ");
+	strcat(buffer, inet_ntoa(serverAddr.sin_addr));
+	printf("Server IP addr: %s\n", inet_ntoa(serverAddr.sin_addr));
+	printf("Server Port is: %d\n", ntohs(serverAddr.sin_port));
+	// Send these data to server
+	if (send(clientSocket, buffer, strlen(buffer), 0) < 0) {
+	    printf("Client send() failed");
+	    return 1;
+	}
 
 	/*---- Read the message from the server into the buffer ----*/
-	recv(clientSocket, buffer, 1024, 0);
+	if (recv(clientSocket, buffer, 1024, 0) < 0) {
+	    printf("recv() function failed");
+	    break;
+	}
 
 	/*---- Print the received message ----*/
 	printf("Data received: %s\n", buffer);
+	i++;
+    }
 
-	return 0;
+    close(clientSocket);
+    return 0;
 }
--- socketTCPIP/server.c
+++ socketTCPIP/server.c
@@ -1,4 +1,5 @@
 /****************** SERVER CODE ****************/
+// https://www.binarytides.com/server-client-example-c-sockets-linux/
 
 #include <stdio.h>
 #include <string.h>
@@ -12,80 +13,83 @@
 
 // From: https://stackoverflow.com/questions/1570511/c-code-to-get-the-ip-address
 void get_ip_address(int socket_fd, struct ifreq *ifr) {
-	// socket_fd = socket(AF_INET, SOCK_DGRAM, IPPROTO_IP);
+    // socket_fd = socket(AF_INET, SOCK_DGRAM, IPPROTO_IP);
 
-	ifr->ifr_addr.sa_family = AF_INET;
-	snprintf(ifr->ifr_name, IFNAMSIZ, "enp0s31f6");
-	ioctl(socket_fd, SIOCGIFADDR, ifr);
+    ifr->ifr_addr.sa_family = AF_INET;
+    snprintf(ifr->ifr_name, IFNAMSIZ, "eth1");
+    ioctl(socket_fd, SIOCGIFADDR, ifr);
 
-	/* and more importantly */
-	printf("Server IP addr: %s\n", inet_ntoa(((struct sockaddr_in *)&ifr->ifr_addr)->sin_addr));
-	printf("Server Port is: %d\n", ntohs(((struct sockaddr_in *)&ifr->ifr_addr)->sin_port));
+    /* and more importantly */
+    printf("Server IP addr: %s\n", inet_ntoa(((struct sockaddr_in *)&ifr->ifr_addr)->sin_addr));
+    printf("Server Port is: %d\n", ntohs(((struct sockaddr_in *)&ifr->ifr_addr)->sin_port));
 
-	// close(socket_fd);
-	// return &ifr;
+    // close(socket_fd);
+    // return &ifr;
 }
 
 int main() {
-	int welcomeSocket, newSocket;
-	char buffer[1024]; //  bzero(&buffer, sizeof(buffer));
-	struct sockaddr_in serverAddr;
-	struct sockaddr_storage serverStorage;
-	socklen_t addr_size;
+    int welcomeSocket, newSocket;
+    char buffer[1024]; //  bzero(&buffer, sizeof(buffer));
+    struct sockaddr_in serverAddr;
+    struct sockaddr_storage serverStorage;
+    socklen_t addr_size;
 
-	bzero(&buffer, sizeof(buffer));
+    bzero(&buffer, sizeof(buffer));
 
-	/*---- Create the socket. The three arguments are: ----*/
-	/* 1) Internet domain 2) Stream socket 3) Default protocol IPPROTO_IP (IP in this case) */
-	// welcomeSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_IP);
-	/* 1) Internet domain 2) Stream socket 3) IPPROTO_TCP (TCP in this case) */
-	welcomeSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-	/* 1) Internet domain 2) Stream socket 3) IPPROTO_UDP (UDP in this case) */
-	// welcomeSocket = socket(PF_INET, SOCK_STREAM, IPPROTO_UDP);
+    /*---- Create the socket. The three arguments are: ----*/
+    /* 1) Internet domain 2) Stream socket 3) Default protocol IPPROTO_IP (IP in this case) */
+    // welcomeSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_IP);
+    /* 1) Internet domain 2) Stream socket 3) IPPROTO_TCP (TCP in this case) */
+    welcomeSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    /* 1) Internet domain 2) Stream socket 3) IPPROTO_UDP (UDP in this case) */
+    // welcomeSocket = socket(PF_INET, SOCK_STREAM, IPPROTO_UDP);
 
-	/*---- Configure settings of the server address struct ----*/
-	/* Address family = Internet */
-	serverAddr.sin_family = AF_INET;
+    /*---- Configure settings of the server address struct ----*/
+    /* Address family = Internet */
+    serverAddr.sin_family = AF_INET;
+    
+    /* Set port number, using htons function to use proper byte order */
+    serverAddr.sin_port = htons(7891);
+    /* Set IP address to all data link interfaces */
+    serverAddr.sin_addr.s_addr = INADDR_ANY;
+    /* Set IP address to host address */
+    //	serverAddr.sin_addr.s_addr = inet_addr("192.168.78.28");
+    /* Set all bits of the padding field to 0 */
+    memset(serverAddr.sin_zero, '\0', sizeof serverAddr.sin_zero);
 
-	/* Set port number, using htons function to use proper byte order */
-	serverAddr.sin_port = htons(7891);
-	/* Set IP address to all data link interfaces */
-	serverAddr.sin_addr.s_addr = INADDR_ANY;
-	/* Set IP address to host address */
-	//	serverAddr.sin_addr.s_addr = inet_addr("192.168.78.28");
-	/* Set all bits of the padding field to 0 */
-	memset(serverAddr.sin_zero, '\0', sizeof serverAddr.sin_zero);
+    /*---- Bind the address struct to the socket ----*/
+    bind(welcomeSocket, (struct sockaddr *) &serverAddr, sizeof(serverAddr));
 
-	/*---- Bind the address struct to the socket ----*/
-	bind(welcomeSocket, (struct sockaddr *) &serverAddr, sizeof(serverAddr));
+    int port = (int) ntohs(serverAddr.sin_port);
+    printf("Port is: %d\n", (int) ntohs(serverAddr.sin_port));
 
-	int port = (int) ntohs(serverAddr.sin_port);
-	printf("Port is: %d\n", (int) ntohs(serverAddr.sin_port));
+    /*---- Listen on the socket, with 5 max connection requests queued ----*/
+    if(listen(welcomeSocket, 5)==0) printf("Listening\n");
+    else printf("Error\n");
 
-	while (1) {
-		/*---- Listen on the socket, with 5 max connection requests queued ----*/
-		if(listen(welcomeSocket, 5)==0) printf("Listening\n");
-		else printf("Error\n");
-
-		/*---- Accept call creates a new socket for the incoming connection ----*/
-		addr_size = sizeof serverStorage;
-		newSocket = accept(welcomeSocket, (struct sockaddr *) &serverStorage, &addr_size);
-
-		/*---- Get the Client IP address ----*/
-		struct sockaddr_in m_addr;
-		socklen_t len = sizeof m_addr;
-		getpeername(newSocket, (struct sockaddr*)&m_addr, &len);
-		strcpy(buffer, "Client IP addr: ");
-		strcat(buffer, inet_ntoa(m_addr.sin_addr));
-		printf("Client IP addr: %s\n", inet_ntoa(m_addr.sin_addr));
-		printf("Client Port is: %d\n", ntohs(m_addr.sin_port));
-
-		/*---- Send message to the socket of the incoming connection ----*/
-		struct ifreq ifr;
-		get_ip_address(newSocket, &ifr);
-		// strcpy(buffer, inet_ntoa(m_addr.sin_addr));
-		send(newSocket, buffer, 64, 0);
+    /*---- Accept call creates a new socket for the incoming connection ----*/
+    addr_size = sizeof serverStorage;
+    newSocket = accept(welcomeSocket, (struct sockaddr *) &serverStorage, &addr_size);
+    int read_size;
+    while (1) {
+	if ((read_size = recv(newSocket, buffer, 64, 0)) > 0 ) {
+	    /*---- Print the received message ----*/
+	    printf("Data received from client: %s\n", buffer);
+	    /* Send message back to client/to the socket of the incoming connection */
+	    /*---- Get the Client IP address ----*/
+	    struct sockaddr_in m_addr;
+	    socklen_t len = sizeof m_addr;
+	    getpeername(newSocket, (struct sockaddr*)&m_addr, &len);
+	    strcpy(buffer, "Client IP addr: ");
+	    strcat(buffer, inet_ntoa(m_addr.sin_addr));
+	    printf("Client IP addr: %s\n", inet_ntoa(m_addr.sin_addr));
+	    printf("Client Port is: %d\n", ntohs(m_addr.sin_port));
+	    struct ifreq ifr;
+	    get_ip_address(newSocket, &ifr);
+	    // strcpy(buffer, inet_ntoa(m_addr.sin_addr));
+	    send(newSocket, buffer, 64, 0);
 	}
+    }
 
-	return 0;
+    return 0;
 }
